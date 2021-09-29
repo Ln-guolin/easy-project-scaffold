d327769a7cb802ab08b2f9676dc2a2f05edf7531
827477620@qq.com <>
2021-09-29 23:25:25 +0800

    init

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO `config_info`(name,url,branch,username,password) VALUES ('demo1', 'http://u.32e.co:8122/git/root/template-bp.git','master', 'root', 'root');
-INSERT INTO `config_info`(name,url,branch,username,password) VALUES ('demo2', 'http://u.32e.co:8122/git/root/template-bp.git','master', 'root', 'root');
+INSERT INTO `config_info`(name,url,branch,username,password) VALUES ('demo1', 'http://u.32e.co:8122/git/root/demo1.git','master', 'root', 'root');
+INSERT INTO `config_info`(name,url,branch,username,password) VALUES ('demo2', 'http://u.32e.co:8122/git/root/demo2.git','master', 'root', 'root');
