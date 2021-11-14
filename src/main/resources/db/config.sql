deb2156424d26d9f6e7d84f6177e158ffcc4948d
827477620@qq.com <>
2021-11-14 08:12:28 +0800

    init

--- src/main/resources/db/config.sql
+++ src/main/resources/db/config.sql
@@ -1,4 +1,4 @@
 -- 模版和分支配置，以及git仓库的账号密码配置，可匿名访问的仓库账号密码可以为空
 INSERT INTO `config_info`(name,url,branch,username,password,enable_def_variable) VALUES ('默认工程变量', 'http://u.32e.co:8122/git/root/demo1.git','master', 'root', 'root',1);
 INSERT INTO `config_info`(name,url,branch,username,password,enable_def_variable,custom_variable) VALUES ('默认工程变量+自定义变量', 'http://u.32e.co:8122/git/root/demo2.git','master', 'root', 'root',1,'prefix,suffix');
-INSERT INTO `config_info`(name,url,branch,username,password,enable_def_variable,custom_variable) VALUES ('自定义变量', 'http://u.32e.co:8122/git/root/demo3.git','master', 'root', 'root',0,'app-name,k8s-namespace,nacos-registry-addr,nacos-namespace,port,k8s-image-pull-secrets');
+INSERT INTO `config_info`(name,url,branch,username,password,enable_def_variable,custom_variable) VALUES ('自定义变量', 'http://u.32e.co:8122/git/root/demo3.git','master', 'root', 'root',0,'app-name,k8s-namespace,nacos-server-addr,nacos-namespace,k8s-image-domain-path,port,k8s-image-pull-secrets,skywalking.agent.namespace');
